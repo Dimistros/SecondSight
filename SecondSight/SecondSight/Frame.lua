5bb1869e261c0b96ebae214f4cfd5110b889b671
Dimistros <jaspar.pahl@gmail.com>
2017-01-04 00:10:01 +0100

    get player stats experiments

--- SecondSight/SecondSight/Frame.lua
+++ SecondSight/SecondSight/Frame.lua
@@ -1,2 +1,43 @@
 -- Author      : Dimistros
 -- Create Date : 1/2/2017 7:08:43 PM
+
+function ExitButton_OnClick()
+	
+end
+
+function GetMissChance()
+
+end
+
+function GetPlayerStats()
+	local pl = "player";
+
+	--base stats:
+	playerStats = {};
+	playerStats["STRENGTH"] = UnitStat(pl, 1);
+	playerStats["AGILITY"] = UnitStat(pl, 2);
+	playerStats["STAMINA"] = UnitStat(pl, 3);
+	playerStats["INTELLECT"] = UnitStat(pl, 4);
+	playerStats["SPIRIT"] = UnitStat(pl, 5);
+	playerStats["ARMOR"] = UnitArmor(pl);
+
+	--special item stats:
+	--like +crit chance, cannot be read from vanilla api to my knowledge
+	--using addon BonusScanner
+	--playerStats["HITBONUS"] = BonusScanner:GetBonus(["TOHIT"]);
+	--playerStats["CRITBONUS"] = BonusScanner:GetBonus(["CRIT"]);
+
+
+	
+
+	return playerStats;
+	
+end
+
+function FrameTemplate_OnLoad()
+	stats = GetPlayerStats();
+	for i in stats do
+		DEFAULT_CHAT_FRAME:AddMessage("BSCAN:".. BonusScanner.active);
+		DEFAULT_CHAT_FRAME:AddMessage(stats[i]);
+	end
+end
